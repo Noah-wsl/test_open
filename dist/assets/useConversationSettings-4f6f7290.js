9b711401edbfb92d05177893b08739e16693e4f3
Noah <noahwsl@163.com>
2026-09-04 21:45:23 +0800

    1

---
+++ dist/assets/useConversationSettings-4f6f7290.js
@@ -0,0 +1,1 @@
+import{y as o}from"./index-db7e1edc.js";function e(){return{conversationStore:o()}}export{e as u};
--- openim-web/config/prod.env.ts
+++ openim-web/config/prod.env.ts
@@ -1,7 +1,7 @@
 const BASE_HOST = 'sdoachat.com'
-const CHAT_URL = `http://${BASE_HOST}:10008`
-const API_URL = `http://${BASE_HOST}:10002`
-const WS_URL = `ws://${BASE_HOST}:10001`
+const CHAT_URL = `https://${BASE_HOST}/api`
+const API_URL = `https://${BASE_HOST}/chat`
+const WS_URL = `wss://${BASE_HOST}/msg_gateway`
 
 export default {
   NODE_ENV: 'production',
