2115117d59003967e0addae4db9bed1d4f1b7ced
Noah <noahwsl@163.com>
2026-09-04 22:29:56 +0800

    1

--- openim-web/config/dev.env.ts
+++ openim-web/config/dev.env.ts
@@ -1,7 +1,7 @@
 const BASE_HOST = 'sdoachat.com'
-const CHAT_URL = `https://${BASE_HOST}/api`
-const API_URL = `https://${BASE_HOST}/im-api`
-const WS_URL = `wss://${BASE_HOST}/msg_gateway`
+const CHAT_URL = `https://${BASE_HOST}/api`        // → /api/         → chat-api 10008
+const API_URL = `https://${BASE_HOST}/im-api`      // → /im-api/      → openim-server 10002
+const WS_URL = `wss://${BASE_HOST}/msg_gateway`    // → /msg_gateway  → msg_gateway 10001
 
 export default {
   NODE_ENV: 'development',
